5204706623e297d8a9510a31d25e2df0878f8c21
Valentin Pichard <7628998+w3st3ry@users.noreply.github.com>
2019-12-03 14:30:04 +0100

    Improve script plugin
    Signed-off-by: Valentin Pichard <7628998+w3st3ry@users.noreply.github.com>

--- pkg/plugins/builtin/script/script.go
+++ pkg/plugins/builtin/script/script.go
@@ -1,7 +1,14 @@
 package script
 
 import (
+	"bytes"
+	"context"
 	"errors"
+	"fmt"
+	gexec "os/exec"
+	"strconv"
+	"syscall"
+	"time"
 
 	"github.com/ovh/utask/pkg/plugins/taskplugin"
 )
@@ -13,19 +20,24 @@ var (
 	)
 )
 
-// Config is the configuration needed to send a ping
+// Config is the configuration needed to execute a script
 type Config struct {
-	File    string
-	Argv    []string
-	Stdin   string
-	Timeout string
+	File    string   `json:"file"`
+	Argv    []string `json:"argv"`
+	Timeout string   `json:"timeout"`
 }
 
 func validConfig(config interface{}) error {
 	cfg := config.(*Config)
 
 	if cfg.File == "" {
-		return errors.New("file field is missing")
+		return errors.New("file is missing")
+	}
+
+	if cfg.Timeout != "" {
+		if _, err := strconv.ParseUint(cfg.Timeout, 10, 64); err != nil {
+			return fmt.Errorf("timeout is wrong %s", err.Error())
+		}
 	}
 
 	return nil
@@ -34,9 +46,53 @@ func validConfig(config interface{}) error {
 func exec(stepName string, config interface{}, ctx interface{}) (interface{}, interface{}, error) {
 	cfg := config.(*Config)
 
-	if cfg.File == "" {
-		return nil, nil, errors.New("file field is missing")
+	var timeout time.Duration
+
+	if cfg.Timeout != "" {
+		t, _ := strconv.ParseInt(cfg.Timeout, 10, 64)
+		timeout = time.Duration(t)
+	} else {
+		timeout = time.Duration(300)
 	}
 
-	return nil, nil, nil
+	ctxe, cancel := context.WithTimeout(context.Background(), timeout*time.Second)
+	defer cancel()
+
+	cmd := gexec.CommandContext(ctxe, cfg.File, cfg.Argv...)
+
+	exitCode := 0
+
+	var errbuf bytes.Buffer
+	cmd.Stderr = &errbuf
+	stderr := errbuf.String()
+
+	out, err := cmd.Output()
+	if err != nil {
+		if exitError, ok := err.(*gexec.ExitError); ok {
+			exitCode = exitError.Sys().(syscall.WaitStatus).ExitStatus()
+		} else {
+			exitCode = 1
+			if stderr == "" {
+				stderr = err.Error()
+			}
+		}
+	} else {
+		exitCode = cmd.ProcessState.Sys().(syscall.WaitStatus).ExitStatus()
+	}
+
+	signal := cmd.ProcessState.Sys().(syscall.WaitStatus).Signal().String()
+
+	payload := struct {
+		ExitCode   string      `json:"exit_code"`
+		ExitSignal string      `json:"exit_signal"`
+		Stdout     interface{} `json:"stdout"`
+		Stderr     interface{} `json:"stderr"`
+	}{
+		ExitCode:   fmt.Sprint(exitCode),
+		ExitSignal: signal,
+		Stdout:     out,
+		Stderr:     []byte(stderr),
+	}
+
+	return payload, cfg, nil
 }
