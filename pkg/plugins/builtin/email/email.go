ovh/utask

commit b7516dc8679edf275036164c7b8b9c9c116144a4

feat: Improve error messages (#51)
Signed-off-by: Valentin Pichard <7628998+w3st3ry@users.noreply.github.com>

pkg/plugins/builtin/email/email.go
@@ -48,8 +48,12 @@ func validConfig(config interface{}) error {
 		return errors.New("smtp_password is missing")
 	}
 
+	if cfg.SMTPPort == "" {
+		return errors.New("smtp_password is missing")
+	}
+
 	if _, err := strconv.ParseUint(cfg.SMTPPort, 10, 64); err != nil {
-		return fmt.Errorf("smtp_port is missing or wrong %s", err.Error())
+		return fmt.Errorf("can't parse smtp_port field %q: %s", cfg.SMTPPort, err.Error())
 	}
 
 	if cfg.SMTPHostname == "" {
@@ -58,7 +62,7 @@ func validConfig(config interface{}) error {
 
 	if cfg.SMTPSkipTLSVerify != "" {
 		if _, err := strconv.ParseBool(cfg.SMTPSkipTLSVerify); err != nil {
-			return fmt.Errorf("smtp_skip_tls_verify is wrong %s", err)
+			return fmt.Errorf("can't parse smtp_skip_tls_verify field %q: %s", cfg.SMTPSkipTLSVerify, err)
 		}
 	}
 
@@ -102,7 +106,7 @@ func exec(stepName string, config interface{}, ctx interface{}) (interface{}, in
 	d := mail.NewDialer(cfg.SMTPHostname, int(port), cfg.SMTPUsername, cfg.SMTPPassword)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: skipTLS, ServerName: cfg.SMTPHostname}
 	if err := d.DialAndSend(message); err != nil {
-		return nil, nil, fmt.Errorf("Send email failed: %s", err.Error())
+		return nil, nil, fmt.Errorf("can't send email: %s", err.Error())
 	}
 
 	return &cfg.mailParameters, nil, nil

pkg/plugins/builtin/apiovh/apiovh.go
@@ -47,61 +47,68 @@ func validConfig(config interface{}) error {
 	switch cfg.Method {
 	case "GET", "POST", "PUT", "DELETE":
 	default:
-		return fmt.Errorf("Unknown method for gw runner: %s", cfg.Method)
+		return fmt.Errorf("unknown method for gw runner: %q", cfg.Method)
 	}
+
 	ovhCfgStr, err := configstore.GetItemValue(cfg.Credentials)
 	if err != nil {
-		return err
+		return fmt.Errorf("can't retrieve credentials from configstore: %s", err)
 	}
+
 	var ovhcfg ovhConfig
 	if err := json.Unmarshal([]byte(ovhCfgStr), &ovhcfg); err != nil {
-		return err
+		return fmt.Errorf("can't unmarshal ovhConfig from configstore: %s", err)
 	}
+
 	if _, err := ovh.NewClient(
 		ovhcfg.Endpoint,
 		ovhcfg.AppKey,
 		ovhcfg.AppSecret,
 		ovhcfg.ConsumerKey); err != nil {
-		return err
+		return fmt.Errorf("can't create new OVH client: %s", err)
 	}
+
 	return nil
 }
 
 func exec(stepName string, config interface{}, ctx interface{}) (interface{}, interface{}, error) {
 	cfg := config.(*APIOVHConfig)
+
 	ovhCfgStr, err := configstore.GetItemValue(cfg.Credentials)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, fmt.Errorf("can't retrieve credentials from configstore: %s", err)
 	}
+
 	var ovhcfg ovhConfig
 	if err := json.Unmarshal([]byte(ovhCfgStr), &ovhcfg); err != nil {
-		return nil, nil, err
+		return nil, nil, fmt.Errorf("can't unmarshal ovhConfig from configstore: %s", err)
 	}
+
 	cli, err := ovh.NewClient(
 		ovhcfg.Endpoint,
 		ovhcfg.AppKey,
 		ovhcfg.AppSecret,
 		ovhcfg.ConsumerKey)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, fmt.Errorf("can't create new OVH client: %s", err)
 	}
 
 	var body interface{}
 	if cfg.Body != "" {
 		reader := bytes.NewReader([]byte(cfg.Body))
 		if err := utils.JSONnumberUnmarshal(reader, &body); err != nil {
-			return nil, nil, err
+			return nil, nil, fmt.Errorf("can't unmarshal body: %s", err)
 		}
 	}
 
 	req, err := cli.NewRequest(cfg.Method, cfg.Path, body, true)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, fmt.Errorf("can't create new request: %s", err)
 	}
 
 	resp, err := cli.Do(req)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, fmt.Errorf("can't execute request: %s", err)
 	}
 
 	return httputil.UnmarshalResponse(resp)

pkg/plugins/builtin/http/http.go
@@ -89,18 +89,18 @@ func validConfig(config interface{}) error {
 	switch cfg.Method {
 	case "GET", "POST", "PUT", "DELETE":
 	default:
-		return fmt.Errorf("Unknown method for HTTP runner: %s", cfg.Method)
+		return fmt.Errorf("unknown method for HTTP runner: %s", cfg.Method)
 	}
 
 	if cfg.TimeoutSeconds != "" {
 		if _, err := strconv.ParseUint(cfg.TimeoutSeconds, 10, 16); err != nil {
-			return fmt.Errorf("timeout_seconds is wrong %s", err.Error())
+			return fmt.Errorf("can't parse timeout_seconds field %q: %s", cfg.TimeoutSeconds, err.Error())
 		}
 	}
 
 	if cfg.DenyRedirects != "" {
 		if _, err := strconv.ParseBool(cfg.DenyRedirects); err != nil {
-			return fmt.Errorf("deny_redirects is wrong %s", err.Error())
+			return fmt.Errorf("can't parse deny_redirects field %q: %s", cfg.DenyRedirects, err.Error())
 		}
 	}
 
@@ -119,7 +119,7 @@ func exec(stepName string, config interface{}, ctx interface{}) (interface{}, in
 
 	req, err := http.NewRequest(cfg.Method, cfg.URL, bytes.NewBuffer(body))
 	if err != nil {
-		return nil, nil, fmt.Errorf("Failed to create http request: %s", err.Error())
+		return nil, nil, fmt.Errorf("failed to create HTTP request: %s", err.Error())
 	}
 
 	q := req.URL.Query()
@@ -154,7 +154,7 @@ func exec(stepName string, config interface{}, ctx interface{}) (interface{}, in
 
 	resp, err := httpClient.Do(req)
 	if err != nil {
-		return nil, nil, fmt.Errorf("HTTP request failed: %s", err.Error())
+		return nil, nil, fmt.Errorf("can't do HTTP request: %s", err.Error())
 	}
 
 	// remove response magic prefix

pkg/plugins/builtin/httputil/httputil.go
@@ -34,7 +34,7 @@ func UnmarshalResponse(resp *http.Response) (interface{}, interface{}, error) {
 
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, nil, fmt.Errorf("failed to read body: %s", err.Error())
+		return nil, nil, fmt.Errorf("can't read body: %s", err.Error())
 	}
 
 	metadata := map[string]interface{}{
@@ -63,7 +63,7 @@ func UnmarshalResponse(resp *http.Response) (interface{}, interface{}, error) {
 		var payload interface{}
 		err = unmarshaler(bodyBytes, &payload)
 		if err != nil {
-			return nil, metadata, fmt.Errorf("failed to unmarshal body: %s", err.Error())
+			return nil, metadata, fmt.Errorf("can't unmarshal body: %s", err.Error())
 		}
 		output = payload
 	} else {

pkg/plugins/builtin/notify/notify.go
@@ -1,8 +1,10 @@
 package notify
 
 import (
+	"errors"
 	"fmt"
 	"sort"
+	"strings"
 
 	"github.com/ovh/utask"
 	"github.com/ovh/utask/pkg/notify"
@@ -31,8 +33,9 @@ func (nc *Config) Message() *notify.Message {
 
 func validConfig(config interface{}) error {
 	cfg := config.(*Config)
+
 	if len(cfg.Backends) == 0 {
-		return nil
+		return errors.New("backends field can't be empty")
 	}
 
 	snames := notify.ListSendersNames()
@@ -43,7 +46,10 @@ func validConfig(config interface{}) error {
 	for _, backend := range cfg.Backends {
 		i := sort.SearchStrings(snames, backend)
 		if i >= len(snames) && snames[i] != backend {
-			return fmt.Errorf("Can't find backend name: %s", backend)
+			return fmt.Errorf(
+				"can't find backend name: %s. Available backends: %s",
+				backend,
+				strings.Join(snames, ", "))
 		}
 	}
 

pkg/plugins/builtin/ping/ping.go
@@ -47,13 +47,13 @@ func validConfig(config interface{}) error {
 
 	if cfg.Count != "" {
 		if _, err := strconv.ParseUint(cfg.Count, 10, 64); err != nil {
-			return fmt.Errorf("count is wrong %s", err.Error())
+			return fmt.Errorf("can't parse count field %q: %s", cfg.Count, err.Error())
 		}
 	}
 
 	if cfg.Interval != "" {
 		if _, err := strconv.ParseUint(cfg.Interval, 10, 64); err != nil {
-			return fmt.Errorf("interval_second is wrong %s", err.Error())
+			return fmt.Errorf("can't parse interval_second field %q: %s", cfg.Interval, err.Error())
 		}
 	}
 
@@ -65,7 +65,7 @@ func exec(stepName string, config interface{}, ctx interface{}) (interface{}, in
 
 	pinger, err := ping.NewPinger(cfg.Hostname)
 	if err != nil {
-		return nil, nil, fmt.Errorf("Initiate ping failed: %s", err.Error())
+		return nil, nil, fmt.Errorf("can't initiate ping: %s", err.Error())
 	}
 
 	pinger.Count = pingDefault(cfg.Count)

pkg/plugins/builtin/script/script.go
@@ -66,7 +66,7 @@ func validConfig(config interface{}) error {
 			return errors.New("timeout must be positive")
 		}
 		if _, err := time.ParseDuration(cfg.Timeout); err != nil {
-			return fmt.Errorf("unable to parse duration %q: %s", cfg.Timeout, err.Error())
+			return fmt.Errorf("can't parse timeout field %q: %s", cfg.Timeout, err.Error())
 		}
 	}
 

pkg/plugins/builtin/ssh/ssh.go
@@ -41,21 +41,27 @@ type ConfigSSH struct {
 
 func configssh(i interface{}) error {
 	cfg := i.(*ConfigSSH)
+
 	if cfg.User == "" {
 		return errors.New("missing ssh username")
 	}
+
 	if cfg.Target == "" {
 		return errors.New("missing ssh target")
 	}
+
 	if cfg.Script == "" {
 		return errors.New("missing ssh script")
 	}
+
 	if cfg.Key == "" {
 		return errors.New("missing ssh key")
 	}
+
 	if len(cfg.Hops) > MaxHops {
 		return fmt.Errorf("ssh too many hops (max %d)", MaxHops)
 	}
+
 	return nil
 }
 
@@ -197,7 +203,7 @@ trap printResultJSON EXIT
 	}
 
 	if exitStatus != 0 && !cfg.AllowExitNonZero {
-		return payload, metadata, fmt.Errorf("Exit code %d", exitStatus)
+		return payload, metadata, fmt.Errorf("exit status code: %d", exitStatus)
 	}
 
 	return payload, metadata, nil

commit 0692d57b7b33206ee5c75ef62265f6648eb170a1

fix: incorrect error message for builtin email pkg introduced by #51
Signed-off-by: Romain Beuque <romain.beuque@corp.ovh.com>

pkg/plugins/builtin/email/email.go
@@ -49,7 +49,7 @@ func validConfig(config interface{}) error {
 	}
 
 	if cfg.SMTPPort == "" {
-		return errors.New("smtp_password is missing")
+		return errors.New("smtp_port is missing")
 	}
 
 	if _, err := strconv.ParseUint(cfg.SMTPPort, 10, 64); err != nil {