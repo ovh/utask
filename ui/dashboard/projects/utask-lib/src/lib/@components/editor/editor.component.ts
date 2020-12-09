09e947bdfa47b5f31801d86a9bd1078d5f0ab712
richardlt <richard.le.terrier@gmail.com>
2020-12-09 18:57:01 +0100

    refactor(ui): improve scroll in code editor

--- ui/dashboard/projects/utask-lib/src/lib/@components/editor/editor.component.ts
+++ ui/dashboard/projects/utask-lib/src/lib/@components/editor/editor.component.ts
@@ -1,24 +1,29 @@
-import { Component, ElementRef, Input, ViewChild, Output, EventEmitter, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild, Output, EventEmitter, OnChanges } from '@angular/core';
 import { EditorOptions } from 'ng-zorro-antd/code-editor';
 
 @Component({
     selector: 'lib-utask-editor',
-    template: `
-        <nz-code-editor class="editor" [ngModel]="ngModel" [nzEditorOption]="config" #editor></nz-code-editor>
-    `,
+    templateUrl: './editor.html',
     styleUrls: ['./editor.sass']
 })
-export class EditorComponent implements AfterViewInit {
+export class EditorComponent implements OnChanges {
     @ViewChild('editor', { static: false }) editor: ElementRef;
-    @Input() config: EditorOptions;
+    @Input() set config(c: EditorOptions) {
+        this._config = {
+            minimap: { enabled: false },
+            scrollBeyondLastLine: false,
+            scrollbar: { alwaysConsumeMouseWheel: false },
+            ...c
+        };
+    }
+    _config: EditorOptions;
     @Input() ngModel: string;
     @Output() ngModelChange = new EventEmitter<string>();
 
     constructor(private elRef: ElementRef) { }
 
-    ngAfterViewInit() {
+    ngOnChanges(): void {
         let height = (this.ngModel.split('\n').length + 1) * 18;
-        height = height > 400 ? 400 : height;
         this.elRef.nativeElement.style.height = `${height}px`;
     }
 }
