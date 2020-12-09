d7070bc18faa417f7b1e13ed6948376d5d60622e
Simon Martinez <1263852+simonmartinez@users.noreply.github.com>
2020-12-09 16:15:06 +0100

    ui: nz editor

--- ui/dashboard/projects/utask-lib/src/lib/@components/editor/editor.component.ts
+++ ui/dashboard/projects/utask-lib/src/lib/@components/editor/editor.component.ts
@@ -4,10 +4,11 @@ import { EditorOptions } from 'ng-zorro-antd/code-editor';
 @Component({
     selector: 'lib-utask-editor',
     template: `
-    <nz-code-editor class="editor" [ngModel]="ngModel" [nzEditorOption]="config"></nz-code-editor> 
+        <nz-code-editor class="editor" [ngModel]="ngModel" [nzEditorOption]="config" #editor></nz-code-editor>
     `,
+    styleUrls: ['./editor.sass']
 })
-export class EditorComponent {
+export class EditorComponent implements AfterViewInit {
     @ViewChild('editor', {
         static: false
     }) editor: ElementRef;
@@ -15,6 +16,12 @@ export class EditorComponent {
     @Input() ngModel: string;
     @Output() ngModelChange = new EventEmitter<string>();
 
-    constructor() {
+    constructor(private elRef: ElementRef) {
+    }
+
+    ngAfterViewInit() {
+        let height = (this.ngModel.split('\n').length + 1) * 18;
+        height = height > 400 ? 400 : height;
+        this.elRef.nativeElement.style.height = `${height}px`;
     }
 }
