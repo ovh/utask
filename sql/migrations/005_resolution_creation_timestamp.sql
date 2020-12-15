d8be87e1d43d78751605997c8226ebc15aa2825c
Arthur Amstutz <18141571+amstuta@users.noreply.github.com>
2020-12-15 09:17:53 +0000

    fix: update default for column created in migration
    Signed-off-by: Arthur Amstutz <18141571+amstuta@users.noreply.github.com>

--- sql/migrations/005_resolution_creation_timestamp.sql
+++ sql/migrations/005_resolution_creation_timestamp.sql
@@ -3,7 +3,7 @@
 -- Adds the resolution creation timestamp on resolution table and sets its value to last_start
 ALTER TABLE "resolution" ADD COLUMN "created" TIMESTAMP with time zone;
 UPDATE "resolution" SET created = last_start;
-ALTER TABLE "resolution" ALTER COLUMN "created" SET NOT NULL;
+ALTER TABLE "resolution" ALTER COLUMN "created" SET NOT NULL, ALTER COLUMN "created" SET DEFAULT now();
 
 -- +migrate Down
 
