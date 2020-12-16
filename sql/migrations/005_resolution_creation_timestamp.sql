9ec1ab46e6054641a9bad0e0290e5a99ce583f42
Romain Beuque <556072+rbeuque74@users.noreply.github.com>
2020-12-16 17:44:57 +0000

    fix: sql migration 1.10.0: handle null last_start on resolution
    When some resolution are created before never run (for example, due to
    TO_AUTORUN_DELAYED, or when a manual resolving is needed), last_start
    property is NULL, which is incompatible with the NOT NULL statement for
    created property. This ALTER corrects the issue.
    
    Signed-off-by: Romain Beuque <556072+rbeuque74@users.noreply.github.com>

--- sql/migrations/005_resolution_creation_timestamp.sql
+++ sql/migrations/005_resolution_creation_timestamp.sql
@@ -3,6 +3,7 @@
 -- Adds the resolution creation timestamp on resolution table and sets its value to last_start
 ALTER TABLE "resolution" ADD COLUMN "created" TIMESTAMP with time zone;
 UPDATE "resolution" SET created = last_start;
+UPDATE "resolution" SET created = NOW() WHERE created IS NULL;
 ALTER TABLE "resolution" ALTER COLUMN "created" SET NOT NULL, ALTER COLUMN "created" SET DEFAULT now();
 
 -- +migrate Down
