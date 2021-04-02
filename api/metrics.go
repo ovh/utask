1e3c6b7e1887fbec91c417619b12b355e4868bd5
Thomas Bétrancourt <rclsilver@gmail.com>
2021-04-02 08:17:39 +0000

    feat(api): allow to filter tasks with tags
    Signed-off-by: Thomas Bétrancourt <rclsilver@gmail.com>

--- api/metrics.go
+++ api/metrics.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/juju/errors"
 	"github.com/loopfz/gadgeto/zesty"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -30,7 +32,7 @@ func collectMetrics(ctx context.Context) {
 		for {
 			select {
 			case <-tick.C:
-				stats, err := task.LoadStateCount(dbp)
+				stats, err := task.LoadStateCount(dbp, nil)
 				if err != nil {
 					logrus.Warn(err)
 				}
@@ -45,6 +47,10 @@ func collectMetrics(ctx context.Context) {
 	}()
 }
 
+type StatsIn struct {
+	Tags []string `query:"tag" explode:"true"`
+}
+
 // StatsOut aggregates different business stats:
 // - a map of task states and their count
 type StatsOut struct {
@@ -53,14 +59,26 @@ type StatsOut struct {
 
 // Stats handles the http request to fetch µtask statistics
 // common to all instances
-func Stats(c *gin.Context) (*StatsOut, error) {
+func Stats(c *gin.Context, in *StatsIn) (*StatsOut, error) {
 	dbp, err := zesty.NewDBProvider(utask.DBName)
 	if err != nil {
 		return nil, err
 	}
 
+	tags := make(map[string]string, len(in.Tags))
+	for _, t := range in.Tags {
+		parts := strings.Split(t, "=")
+		if len(parts) != 2 {
+			return nil, errors.BadRequestf("invalid tag %s", t)
+		}
+		if parts[0] == "" || parts[1] == "" {
+			return nil, errors.BadRequestf("invalid tag %s", t)
+		}
+		tags[parts[0]] = parts[1]
+	}
+
 	out := StatsOut{}
-	out.TaskStates, err = task.LoadStateCount(dbp)
+	out.TaskStates, err = task.LoadStateCount(dbp, tags)
 	if err != nil {
 		return nil, err
 	}
