fac4e9e294c8d9dfd66acb7d1b6e2d297ee0873b
Thomas Bétrancourt <thomas@betrancourt.net>
2022-10-18 13:49:28 +0000

    chore: allow to override db schema and docker network in tests
    Signed-off-by: Thomas Bétrancourt <thomas@betrancourt.net>

--- hack/test-docker.sh
+++ hack/test-docker.sh
@@ -4,12 +4,35 @@ export PG_USER="test"
 export PG_PASSWORD="test"
 export PG_PORT="5432"
 export PG_DATABASENAME="test"
+export PG_SCHEMA=${PG_SCHEMA:-${PWD}/sql/schema.sql}
 
-PG_DOCKER_ID=$(docker run -v $PWD/sql/schema.sql:/docker-entrypoint-initdb.d/v0001.sql:ro -e POSTGRES_USER=$PG_USER -e POSTGRES_PASSWORD=$PG_PASSWORD -e POSTGRES_DBNAME=$PG_DATABASENAME -d postgres:14-alpine)
+ENV_FILE=$(readlink -f $(dirname ${0}))/test.env
+
+if [ -f ${ENV_FILE} ]; then
+    source ${ENV_FILE}
+fi
+
+DOCKER_OPTS=$(cat <<EOF
+-v ${PG_SCHEMA}:/docker-entrypoint-initdb.d/v0001.sql:ro
+-e POSTGRES_USER=${PG_USER}
+-e POSTGRES_PASSWORD=${PG_PASSWORD}
+-e POSTGRES_DBNAME=${PG_DATABASENAME}
+EOF
+)
+
+if [ ! -z "${PG_NETWORK}" ]; then
+    DOCKER_OPTS="${DOCKER_OPTS} --network ${PG_NETWORK}"
+fi
+
+PG_DOCKER_ID=$(docker run ${DOCKER_OPTS} -d postgres:14-alpine)
 
 echo "Spawned Postgres docker: $PG_DOCKER_ID"
 
-export PG_HOST=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $PG_DOCKER_ID)
+if [ -z "${PG_NETWORK}" ]; then
+    export PG_HOST=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $PG_DOCKER_ID)
+else
+    export PG_HOST=$(docker inspect --format "{{ (index .NetworkSettings.Networks \"${PG_NETWORK}\").IPAddress }}" $PG_DOCKER_ID)
+fi
 
 echo "Wait for database init..."
 hack/wait-for-it/wait-for-it.sh -t 120 ${PG_HOST}:${PG_PORT}
