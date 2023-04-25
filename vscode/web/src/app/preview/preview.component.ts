865467f767600d275c9beea92693ce2a3713f66b
Thomas Bétrancourt <thomas@betrancourt.net>
2023-04-25 11:48:42 +0000

    fix(vscode): display errors and fix graph edge backgrounds
    Signed-off-by: Thomas Bétrancourt <thomas@betrancourt.net>

--- vscode/web/src/app/preview/preview.component.ts
+++ vscode/web/src/app/preview/preview.component.ts
@@ -1,6 +1,6 @@
-import { AfterViewInit, Component, HostListener, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, HostListener, ViewChild } from '@angular/core';
 import { StepsViewerComponent } from '@ovhcloud/utask-lib';
-import Step from '@ovhcloud/utask-lib/lib/@models/step.model';
+import Resolution from '@ovhcloud/utask-lib/lib/@models/resolution.model';
 import { parse } from 'yaml';
 
 @Component({
@@ -12,23 +12,46 @@ export class PreviewComponent implements AfterViewInit {
   // @ts-ignore
   private _vscode = acquireVsCodeApi();
 
-  @ViewChild(StepsViewerComponent, { static: true }) viewer!: StepsViewerComponent;
+  resolution: Resolution | null = null;
+  invalid = false;
+
+  @ViewChild(StepsViewerComponent, { static: false }) viewer!: StepsViewerComponent;
+
+  constructor(private _changeDetectorRef: ChangeDetectorRef){}
 
   ngAfterViewInit(): void {
     this._vscode.postMessage({
       type: 'initialized',
       value: true,
-    })
+    });
+  }
+
+  private setResolution(resolution: Resolution | null): void {
+    this.resolution = resolution;
+    this._changeDetectorRef.detectChanges();
+  }
+
+  private clearResolution(): void {
+    this.setResolution(null);
+  }
+
+  private setInvalid(invalid: boolean): void {
+    this.invalid = invalid;
+    this._changeDetectorRef.detectChanges();
   }
 
   @HostListener('window:message', ['$event'])
   onRefresh(e: MessageEvent) {
     if (e.data.type === 'refresh') {
+      this.setInvalid(false);
+      this.clearResolution();
+
       try {
-        this.viewer.resolution = parse(e.data.value);
-        this.viewer.clear();
+        this.setResolution(parse(e.data.value) as Resolution);
+        this.viewer.fit();
         this.viewer.draw();
       } catch (e) {
+        this.setInvalid(true);
         console.error(e);
       }
     }
